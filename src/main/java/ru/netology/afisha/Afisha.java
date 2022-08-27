33c72cc62ee2bc3ca1e8400fa0028795c7b962e3
murin <murin.stanislav@mail.ru>
2022-08-27 10:49:41 +0300

    тесты работают,покрытие проверил.

--- src/main/java/ru/netology/afisha/Afisha.java
+++ src/main/java/ru/netology/afisha/Afisha.java
@@ -1,11 +1,9 @@
 package ru.netology.afisha;
 
 
-
 public class Afisha {
-
-    private String[] movies= new String[0];//создаем массив
-    private int limit;//заводим поле лимита фильмов
+    protected String[] movies = new String[0];//создаем массив
+    protected int limit;//заводим поле лимита фильмов
 
     public Afisha() {
         limit = 10;
@@ -15,12 +13,15 @@ public class Afisha {
         this.limit = limit;
     }
 
-    public void add(String movie) {  //метод добавления
-        String[] tmp = new String[movies.length +1];//создаем новый массив,который на оду ячейку больше старого
+    public void add(String movie) {  //метод добавления фильмов
+        String[] tmp = new String[movies.length + 1];//создаем новый массив,который на оду ячейку больше старого
         for (int i = 0; i < movies.length; i++) { //копируем всю информацию из старого массива в новый
-            tmp[i] = movies[1];
+            tmp[i] = movies[i];
+
+            // for(int i = 0; i < movies.length; i++) { //копируем всю информацию из старого массива в новый
+            tmp[i] = movies[i];
         }
-        tmp[tmp.length -1] = movie;// в tmp лежит старый массив,но на еденицу больше
+        tmp[tmp.length - 1] = movie;// в tmp лежит старый массив,но на еденицу больше
         movies = tmp;
 
     }
@@ -30,19 +31,25 @@ public class Afisha {
 
     }
 
-    public String[] findLast() { //метод показать последний постер
+    public String[] findLast() { //метод показать последний добавленый постер
 
-        int length;//заводим переменную длины афиши
-        if (movies.length < limit) {//если фильмов меньше 10,то
-            length = movies.length;//,то выводим количество фильмов
-        } else {
-            length = limit;//иначе выводим десять фильмов
-        }
-        String[] tmp = new String[length]; //создаем массив на кол-во  фильмов
+        int lengthMovies = Math.min(movies.length, limit);//заводим переменную длины афиши(кол-во фильмов)
+
+//        int lengthMovies;//заводим переменную длины афиши(кол-во фильмов)
+//        if (movies.length < limit) { //если фильмов меньше лимита 10,то
+//            lengthMovies = movies.length;//,то выводим количество фильмов
+//        } else {
+//            lengthMovies = limit;}//иначе выводим десять фильмов
+
+
+        String[] tmp = new String[lengthMovies]; //создаем массив на кол-во  фильмов
         for (int i = 0; i < tmp.length; i++) { //проходим по массиву
-            tmp[i] = movies[movies.length -1 -i];}
+            tmp[i] = movies[movies.length - 1 - i];
+        }
         return tmp;
 
-        }
+    }
 }
+
+
 
--- src/test/java/ru/netology/afisha/AfishaTest.java
+++ src/test/java/ru/netology/afisha/AfishaTest.java
@@ -4,23 +4,103 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AfishaTest {
+    @Test
+
+    public void testAddMovies3() {//добавляем 3 фильма и выводим их.
+        Afisha afisha = new Afisha();
+        afisha.add("Movie1");
+        afisha.add("Movie2");
+        afisha.add("Movie3");
+
+        String[] expected = {"Movie1", "Movie2", "Movie3"};
+        String[] actual = afisha.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 
     @Test
 
-    public void testAddMovies() {
-
-
+    public void testFindAll() {//добавляем 13 фильмов и выводим все.
         Afisha afisha = new Afisha();
+        afisha.add("movie1");
+        afisha.add("movie2");
+        afisha.add("movie3");
+        afisha.add("movie4");
+        afisha.add("movie5");
+        afisha.add("movie6");
+        afisha.add("movie7");
+        afisha.add("movie8");
+        afisha.add("movie9");
+        afisha.add("movie10");
+        afisha.add("movie11");
+        afisha.add("movie12");
+        afisha.add("movie13");
 
+        String[] expected = {"movie1", "movie2", "movie3", "movie4", "movie5", "movie6", "movie7", "movie8", "movie9", "movie10", "movie11", "movie12", "movie13"};
+        String[] actual = afisha.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLast10() { //вывести последние 10 фильмов(всего 13),лимит можно менять.(конструктор с переменной лимит.)
+        Afisha afisha = new Afisha(10);
         afisha.add("movie 1");
         afisha.add("movie 2");
         afisha.add("movie 3");
+        afisha.add("movie 4");
+        afisha.add("movie 5");
+        afisha.add("movie 6");
+        afisha.add("movie 7");
+        afisha.add("movie 8");
+        afisha.add("movie 9");
+        afisha.add("movie 10");
+        afisha.add("movie 11");
+        afisha.add("movie 12");
+        afisha.add("movie 13");
 
-        String[] expected = {"movie 1", "movie 2", "movie 3"};
-        String[] actual = afisha.findAll();
+        String[] expect = {"movie 13", "movie 12", "movie 11", "movie 10", "movie 9", "movie 8", "movie 7", "movie 6", "movie 5", "movie 4"};
+        String[] actual = afisha.findLast();
+        Assertions.assertArrayEquals(expect, actual);
+
+    }
+
+    @Test
+    public void findLast1() { //вывести последний добавленный фильм(всего 13),лимит можно менять.(конструктор с переменной лимит.)
+        Afisha afisha = new Afisha(1);
+        afisha.add("movie 1");
+        afisha.add("movie 2");
+        afisha.add("movie 3");
+        afisha.add("movie 4");
+        afisha.add("movie 5");
+        afisha.add("movie 6");
+        afisha.add("movie 7");
+        afisha.add("movie 8");
+        afisha.add("movie 9");
+        afisha.add("movie 10");
+        afisha.add("movie 11");
+        afisha.add("movie 12");
+        afisha.add("movie 13");
+
+        String[] expect = {"movie 13"};
+        String[] actual = afisha.findLast();
+        Assertions.assertArrayEquals(expect, actual);
+
+    }
+
+    @Test
+    public void findLast5() { //лимит 10,но фильмов всего 5,то выводим сколько есть.
+        Afisha afisha = new Afisha(10);
+        afisha.add("movie 1");
+        afisha.add("movie 2");
+        afisha.add("movie 3");
+        afisha.add("movie 4");
+        afisha.add("movie 5");
 
 
-        Assertions.assertArrayEquals(expected, actual);
+        String[] expect = {"movie 5", "movie 4", "movie 3", "movie 2", "movie 1"};
+        String[] actual = afisha.findLast();
+        Assertions.assertArrayEquals(expect, actual);
 
     }
 }
